024d66e73a59f7f8d7460a2f0a541fb739d7ef36
Ved897 <ved2809@yahoo.com.ar>
2016-04-12 04:53:10 +0200

    agregado de las funciones del juego Agilidad Aritmetica, se añade otra funcion llamada mostrar alerta

--- jsAgilidadAritmetica1.js
+++ jsAgilidadAritmetica1.js
@@ -10,11 +10,54 @@ var respuesta;
 function comenzar()
 {
 
+	respuesta = 0;
+	numeroUno =Math.floor( Math.random()*10)+1;
+	document.getElementById('PrimerNumero').value=numeroUno;
 	
+	numeroDos =Math.floor( Math.random()*10)+1;
+	document.getElementById('SegundoNumero').value=numeroDos;
+
+	numeroOperacion =Math.floor( Math.random()*4)+1;
+	
+		switch(numeroOperacion)
+		{
+			case 1://suma
+				document.getElementById('Operador').value='+';
+				respuesta = numeroUno + numeroDos ;
+				break;
+			case 2://resta
+				document.getElementById('Operador').value='-';
+				respuesta = numeroUno - numeroDos ;
+				break;
+			case 3://division
+				document.getElementById('Operador').value='/';
+				respuesta = numeroUno / numeroDos ;	
+
+				if(!Number.isInteger(respuesta))
+				{
+					respuesta = respuesta.toFixed(2);		
+//				    alert (respuesta);
+				}
+				
+				
+				break;
+			case 4://multiplicacion
+				document.getElementById('Operador').value='*';
+				respuesta = (numeroUno * numeroDos);
+				break;
+		}
 
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
+	respUsuario=document.getElementById('Respuesta').value; 
+	if (respUsuario == respuesta)
+	{
+		alert("El resultado es correcto... ");
+		exit();
+	} else { alert("El resultado no es correcto... "); 
+				exit();}
 	
+//    comenzar();
 
 }//FIN DE LA FUNCIÓN
--- jsAgilidadAritmetica2.js
+++ jsAgilidadAritmetica2.js
@@ -7,11 +7,56 @@ var respuesta;
 var temporizador;
 function comenzar()
 {
+		respuesta = 0;
+	numeroUno =Math.floor( Math.random()*10)+1;
+	document.getElementById('PrimerNumero').value=numeroUno;
 	
+	numeroDos =Math.floor( Math.random()*10)+1;
+	document.getElementById('SegundoNumero').value=numeroDos;
+
+	numeroOperacion =Math.floor( Math.random()*4)+1;
+	
+		switch(numeroOperacion)
+		{
+			case 1://suma
+				document.getElementById('Operador').value='+';
+				respuesta = numeroUno + numeroDos ;
+				break;
+			case 2://resta
+				document.getElementById('Operador').value='-';
+				respuesta = numeroUno - numeroDos ;
+				break;
+			case 3://division
+				document.getElementById('Operador').value='/';
+				respuesta = numeroUno / numeroDos;
+				break;
+			case 4://multiplicacion
+				document.getElementById('Operador').value='*';
+				respuesta = numeroUno * numeroDos ;
+				break;
+		}
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
 	
+temporizador = 5000;
+	setTimeout(mostrarAlerta, temporizador);
+	alert(setTimeout());
 
+	
+		respUsuario=document.getElementById('Respuesta').value; 
+		if (respUsuario == respuesta)
+		{
+			alert("El resultado es correcto... ");
+		} else 
+				{ 
+					alert("El resultado no es correcto... "); 
+				}		
 
 }//FIN DE LA FUNCIÓN
+
+function mostrarAlerta() 
+{
+	alert('Han pasado 5 segundos desde la carga de la página');
+	comenzar();
+}
