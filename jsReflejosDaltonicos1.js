10b9a1f06b16bd19d10ab0a8aaf4fee12feaadf8
Ved897 <ved2809@yahoo.com.ar>
2016-04-12 04:59:24 +0200

    agregado y modificacion de las funciones del juego Reflejos  Daltonicos. Modificacion en calculo del tiempo

--- jsReflejosDaltonicos1.js
+++ jsReflejosDaltonicos1.js
@@ -8,13 +8,59 @@ var ColorSecreto;
 var tiempoInicio;
 function comenzar()
 {
+	EleccionColorSecreto =Math.floor( Math.random()*6)+1;
+		
+		switch(EleccionColorSecreto)
+		{
+			case 1:
+				
+				ColorSecreto="rojo";
+				break;
+			case 2:
+				ColorSecreto="azul";
+				break;
+			case 3:
+				ColorSecreto="verde";
+				break;
+			case 4:
+				ColorSecreto="amarillo";
+				break;
+			case 5:
+				ColorSecreto="marron";
+				break;
+			case 6:
+				ColorSecreto="celeste";
+				break;
+			
 
+		}
+		
+	document.getElementById('ColorElejido').value=ColorSecreto;
+
+	tiempoInicio=  new Date();
+// 	tiempoInicio=tiempoInicio.getTime();
+	tiempoInicio = Math.round(tiempoInicio.getTime()/1000);
+	
 	
 
 }//FIN DE LA FUNCIÓN
 function Responder(colorParametro)
 {
-	
+	alert(tiempoInicio);
+	if(colorParametro==ColorSecreto)
+		{
+//			tiempoFinal= new Date();
+//			tiempoFinal=tiempoFinal.getTime();
+			tiempoFinal = Math.round((new Date()).getTime()/1000)
+			resultado=tiempoFinal-tiempoInicio;
+			alert("Su tiempo fue: "+resultado + " segundos");
+		}
+		else
+		{
+			alert("Boton Incorrecto");
+		}
+
+
 	
 
 
