4b7cf718f38761e714f2ad17b49cbfd8df0e343e
Ved897 <ved2809@yahoo.com.ar>
2016-04-12 04:57:02 +0200

    agregado de las funciones y modificacion de la parte logica del juego Piedra Papel o Tijera

--- jsPiedarPapelTijera1.js
+++ jsPiedarPapelTijera1.js
@@ -8,22 +8,82 @@ var eleccionMaquina;
 
 function comenzar()
 {
-	
+	numeroSecreto =Math.floor( Math.random()*3)+1;	
+		switch(numeroSecreto)
+		{
+			case 1:
+				eleccionMaquina="piedra";
+				break;
+			case 2:
+				eleccionMaquina="papel";
+				break;
+			case 3:
+				eleccionMaquina="tijera";
+				break;
+		}
+	//alert(eleccionMaquina);	
 
 
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+	alert("La maquina selecciono: "+eleccionMaquina);
+	eleccionHumano="piedra";
+
+	if(eleccionHumano==eleccionMaquina)
+	{
+		alert("Empató.");	
+	}
+	else if(eleccionMaquina=="tijera")
+	{
+		alert("Ganó.");
+	}
+	else
+	{
+		alert("Perdió.");	
+	}
+
+//comenzar();
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
+	alert("La maquina selecciono: "+eleccionMaquina);
+	eleccionHumano="papel";
+
+	if(eleccionHumano==eleccionMaquina)
+	{
+		alert("Empató.");
+
+	}
+	else if(eleccionMaquina=="piedra")
+	{
+		alert("Ganó.");
+	}
+	else
+	{
+		alert("Perdió.");
+	}
+
 
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	alert("La maquina selecciono: "+eleccionMaquina);
+	eleccionHumano="tijera";
+
+	if(eleccionHumano==eleccionMaquina)
+	{
+		alert("Empató.");
+	}
+	else if(eleccionMaquina=="papel")
+	{
+		alert("Ganó.");
+	}
+	else
+	{
+		alert("Perdió.");
+	}
 
 }//FIN DE LA FUNCIÓN
